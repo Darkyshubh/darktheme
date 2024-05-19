a5ec86b294f71347a2b660782bb57c46ddae7bd2
Dark-X369 <157467612+Dark-X369@users.noreply.github.com>
2024-05-20 03:09:08 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,34 @@
+#bin/bash/'!¡
+echo ""
+echo ""
+echo "==================="
+echo "Installing Dark Banner"
+echo "==================="
+echo "Installing Packages"
+echo "==================="
+echo "Made By @Dark_X369"
+echo "==================="
+xdg-open https://t.me/Dark_X369
+echo ""
+apt update
+pkg install python -y
+apt upgrade
+pip install lolcat
+pkg install mpv -y
+pkg install figlet -y
+cp bash.bashrc $PREFIX/etc
+clear
+echo ""
+echo ""
+
+echo "Installed All Packages" |lolcat
+echo "========================="
+echo "Ready To Go.........." |lolcat
+echo "========================="
+echo "Done By @Dark_X369" |lolcat
+echo "========================="
+echo "Restart Your Termux App" |lolcat
+echo "========================="
+echo ""
+xdg-open https://www.youtube.com/@Dark-X369
+exit
